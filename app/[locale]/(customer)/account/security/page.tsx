0e07e533c82e50cbecb2eccc4909663bdaefc204
manshu145 <manshu.ibc24@gmail.com>
2026-09-21 18:14:05 +0530

    style: apply formatter output

--- app/[locale]/(customer)/account/security/page.tsx
+++ app/[locale]/(customer)/account/security/page.tsx
@@ -2,5 +2,9 @@ import { PageShell } from '@/components/layout/page-shell';
 import { SecurityActions } from '@/components/customer/security-actions';
 
 export default function SecurityPage() {
-  return <PageShell title="Security"><SecurityActions /></PageShell>;
+  return (
+    <PageShell title="Security">
+      <SecurityActions />
+    </PageShell>
+  );
 }
